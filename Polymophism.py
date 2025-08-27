19e22881c10188564ea6c35653c87dc9b131de33
Bblasio <ochiengblasio@gmail.com>
2025-08-28 02:42:28 +0300

    week 5 submission

--- Polymophism.py
+++ Polymophism.py
@@ -6,17 +6,17 @@ class Vehicle:
 # Subclass: Car
 class Car(Vehicle):
     def move(self):
-        print("🚗 Driving on the road...")
+        print(" Driving on the road...")
 
 # Subclass: Plane
 class Plane(Vehicle):
     def move(self):
-        print("✈️ Flying in the sky...")
+        print(" Flying in the sky...")
 
 # Subclass: Boat
 class Boat(Vehicle):
     def move(self):
-        print("🚤 Sailing on water...")
+        print(" Sailing on water...")
 
 # Example usage
 vehicles = [Car(), Plane(), Boat()]
